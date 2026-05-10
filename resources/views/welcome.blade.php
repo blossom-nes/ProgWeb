cdec3d3b007886242c6c244ec48e75a3e24c6948
Ynes <Ynesmbonguembappe@gmail.com>
2026-05-10 15:19:30 +0200

    beugs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,21 +7,21 @@
 
     <div class="row justify-content-center g-3">
         <div class="col-md-4">
-            <div class="card p-4">
+            <div class="card p-4 h-100 w-100">
                 <h5> Calendrier</h5>
                 <p class="text-muted small">Consultez le programme des compétitions par discipline et par date.</p>
                 <a href="/calendrier" class="btn btn-outline-secondary">Voir le calendrier</a>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card p-4">
+            <div class="card p-4 h-100 w-100">
                 <h5> Billetterie</h5>
                 <p class="text-muted small">Achetez vos billets pour assister aux épreuves en direct.</p>
                 <a href="/competitions" class="btn btn-outline-secondary">Réserver des billets</a>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card p-4">
+            <div class="card p-4 h-100 w-100" >
                 <h5>Mon compte</h5>
                 <p class="text-muted small">Connectez-vous pour gérer vos réservations.</p>
                 @auth
